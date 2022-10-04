5d5f910c0b2eb63b50f8962f2102ad954d0503bc
mdominguezg <mdominguezg@106-06.info2>
2022-10-04 10:59:47 +0200

    commit

--- EjerciciosTema1/src/ejerciciostema1/Ejercicio10.java
+++ EjerciciosTema1/src/ejerciciostema1/Ejercicio10.java
@@ -1,10 +1,22 @@
 package ejerciciostema1;
 
+import java.util.Scanner;
+
 public class Ejercicio10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//bloque de variables
+		double metros;					// la distancia inicial en m 
+		double centimetrosfinales;		//sera el resultado final del lanzamiento
+		
+		//creamos el scanner y el solicitamos al cliente la cantidad de m lanzados
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Introduzca la distacia del lanzamiento en m");
+		metros=sc.nextDouble();
+		centimetrosfinales=metros*100;
+		// una vez pasado los m a cm mostramos al cliente su lanzamiento en numeros enteros
+		System.out.print("Su lanzamiento ha sido de: "+(int)centimetrosfinales+"cm");
+		sc.close();
 	}
 
 }
--- EjerciciosTema1/src/ejerciciostema1/Ejercicio6.java
+++ EjerciciosTema1/src/ejerciciostema1/Ejercicio6.java
@@ -6,21 +6,23 @@ public class Ejercicio6 {
 
 	public static void main(String[] args) {
 		// bloque de variables
-		int segundos,segundosintroducidos; //se guardaran los segundos introducidos
+		int segundosintroducidos; //se guardaran los segundos introducidos
 		int minutos,horas; // se guardaran las horas y los minutos que resulten esos segundos
+		int restoh,restom;
 		// creamos el scanner y solicitamos los segundos
 		Scanner sc= new Scanner(System.in);
 		System.out.print("Introduzca la cantidad de segundos: ");
 		segundosintroducidos=sc.nextInt();
 		// calculamos la cantidad de minutos y horas que son esos segundos
-		horas= segundosintroducidos/3600;
-		minutos = horas/60;
-		segundos = minutos/60;
+		horas= (segundosintroducidos/3600);
+		restoh=segundosintroducidos%3600;
+		minutos = (restoh/60);
+		restom=restoh%60;
 		// mostgramos al cliente el resultado
 		System.out.println(segundosintroducidos+" segundos son : ");
 		System.out.println((int)horas+"horas");
 		System.out.println((int) minutos+" minutos");
-		System.out.println( segundos+" segundos");
+		System.out.println( restom+" segundos");
 		sc.close();
 	}
 
